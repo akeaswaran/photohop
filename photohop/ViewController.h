b6223597b618cb1a50d34cd9d3e5533cf0483338
Akshay Easwaran <akeaswaran@me.com>
2016-02-11 11:55:40 -0500

    Initial Commit - mainly date checking logic

--- photohop/ViewController.h
+++ photohop/ViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+@import Photos;
+
 @interface ViewController : UIViewController
 
 
--- photohop/ViewController.m
+++ photohop/ViewController.m
@@ -7,15 +7,38 @@
 //
 
 #import "ViewController.h"
+#import <Photos/Photos.h>
 
 @interface ViewController ()
-
+@property (strong, nonatomic) NSDate *today;
+@property (strong, nonatomic) PHFetchResult *images;
+@property (strong, nonatomic) NSMutableArray *todayMedia;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blueColor];
+    _today = [NSDate date];
+    _todayMedia = [NSMutableArray array];
+    _images = [PHAsset fetchAssetsWithMediaType:PHAssetMediaTypeImage options:nil];
+    for (PHAsset *asset  in _images) {
+        PHContentEditingInputRequestOptions *options = [[PHContentEditingInputRequestOptions alloc] init];
+        options.networkAccessAllowed = YES;
+        
+        [asset requestContentEditingInputWithOptions:options completionHandler:^(PHContentEditingInput *contentEditingInput, NSDictionary *info) {
+            CIImage *fullImage = [CIImage imageWithContentsOfURL:contentEditingInput.fullSizeImageURL];
+            NSLog(@"FULL IMAGE CREATION DATE STRING: %@", fullImage.properties[@"{TIFF}"][@"DateTime"]);
+            NSDate *creationDate = fullImage.properties[@"{TIFF}"][@"DateTime"];
+            NSLog(@"CREATION DATE: %@", creationDate);
+            NSDateComponents *creationDateComps = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitMonth fromDate:creationDate];
+            NSDateComponents *todayDateComps = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitMonth fromDate:_today];
+            if (todayDateComps.month == creationDateComps.month && todayDateComps.day == creationDateComps.day) {
+                [_todayMedia addObject:asset];
+            }
+        }];
+    }
     // Do any additional setup after loading the view, typically from a nib.
 }
 
